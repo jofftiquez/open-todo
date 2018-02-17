d7a53830af62eb77f4fbcecc2388ee079ebdd5ad
Joff Tiquez <jofftiquez@gmail.com>
2018-02-17 16:02:58 +0800

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,6 @@ export default {
   font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/dashboard.vue
+++
@@ -1,5 +0,0 @@
-<template lang="pug">
-  div
-    h1 Open Todo v0.0.1
-    v-btn.green--text foo
-</template>
