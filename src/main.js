abd19e98dd7f3956afd9639fae1a2e40dac2d7f6
Joff Tiquez <jofftiquez@gmail.com>
2018-02-17 16:02:38 +0800

    ➕ Added new dependencies

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import store from './vuex';
+import './firebase';
+import 'babel-polyfill';
+import colors from 'vuetify/es5/util/colors';
+
+console.log(colors)
  
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  theme: store.getters['config/theme']
+});
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 });
