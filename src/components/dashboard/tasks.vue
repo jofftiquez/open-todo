c207a90df0f2c932435d9fff9bc79ce009e2cffa
Joff Tiquez <jofftiquez@gmail.com>
2018-02-22 01:25:19 +0800

    Added query and real task data

--- src/components/dashboard/tasks.vue
+++ src/components/dashboard/tasks.vue
@@ -11,31 +11,7 @@
             v-footer.pa-1 
               strong Today
             v-card-text(style="height: 400px; overflow: auto;").pa-2
-              v-card(flat v-for="(x, i) in [0,1,2,3,4,5,6]" :key="i").elevation-1.mb-2
-                v-card-text.pa-2
-                  small Title
-                  p 
-                    small Description
-                  small 10 Subtasks
-                v-divider
-                v-card-actions
-                  v-tooltip(bottom)
-                    v-btn(icon slot="activator").primary--text 
-                      v-icon done
-                    | Mark as done
-                  v-tooltip(bottom)
-                    v-btn(icon slot="activator").primary--text 
-                      v-icon remove_red_eye
-                    | View
-                  v-tooltip(bottom)
-                    v-btn(icon slot="activator").primary--text 
-                      v-icon color_lens
-                    | Change Color
-                  v-spacer
-                  v-tooltip(bottom)
-                    v-btn(icon slot="activator").primary--text 
-                      v-icon archive
-                    | Archive
+              task-item-card(v-for="(task, i) in tasks"  :key="i" :item="task")
         v-flex(xs12 md4).pa-1
           v-card
             v-footer.pa-1 
@@ -46,21 +22,54 @@
             strong Done
           v-card
             v-card-text
-            
+    v-tooltip(top) 
+      v-btn(
+        color="primary"
+        slot="activator"
+        fab
+        bottom
+        right
+        fixed
+        @click.stop="addTaskDialog = true"
+      )
+        v-icon add
+      | Add Task
+    create-task(
+      :dialog="addTaskDialog"
+      @close="val => addTaskDialog = val"
+    )
 </template>
 
 <script>
+  import TaskItemCard from '../commons/task-item-card';
+  import CreateTask from '../dialogs/create-task';
   export default {
+    created() {
+      this.$store.dispatch('tasks/streamTasks');
+    },
+    components: {
+      TaskItemCard,
+      CreateTask
+    },
+    data() {
+      return {
+        addTaskDialog: false
+      }
+    },
     computed: {
       binding () {
         const binding = {};
-
         if (this.$vuetify.breakpoint.mdAndUp) {
           binding.row = true;
         } else {
           binding.column = true;
         }
         return binding
+      },
+      tasks: {
+        get() {
+          return this.$store.getters['tasks/tasks'];
+        }
       }
     }
   }
